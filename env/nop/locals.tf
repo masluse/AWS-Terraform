e03c9ad4e9d4e70c7bf89cb609943a7ed0652a97
Manuel Regli <manuel.1231231234@gmail.com>
2023-11-08 07:22:47 +0000

    Make local.tf and modules.tf better

--- env/nop/locals.tf
+++ env/nop/locals.tf
@@ -1,29 +1,37 @@
 # Define local variables for reuse within this Terraform module.
 locals {
+  # Region for the Instances and configuration that will be created
   aws_region = "us-east-1"
   # VM (Virtual Machine) configuration to specify the compute resources.
   vm1_name      = "vm-1"                  # Name identifier for the virtual machine.
   vm1_type      = "t2.nano"               # Machine type specifying a particular CPU/RAM configuration.
   vm1_image     = "ami-0fc5d935ebf8bc3bc" # OS image for the VM.
   vm1_disk_size = 8                       # Size of the VM's primary disk in GB.
-  vm1_disk_type = "standard"
+  vm1_disk_type = "standard"              # Type of the VM's primary disk. (standard / gp2 / gp3 / io1 / io2 / sc1 / st1)
 
   # VM (Virtual Machine) configuration to specify the compute resources.
   vm2_name      = "vm-2"                  # Name identifier for the virtual machine.
   vm2_type      = "t2.nano"               # Machine type specifying a particular CPU/RAM configuration.
   vm2_image     = "ami-0fc5d935ebf8bc3bc" # OS image for the VM.
   vm2_disk_size = 8                       # Size of the VM's primary disk in GB.
-  vm2_disk_type = "standard"
+  vm2_disk_type = "standard"              # Type of the VM's primary disk. (standard / gp2 / gp3 / io1 / io2 / sc1 / st1)
 
-  vpc1_name = "main-vpc"
-  vpc1_cidr = "172.20.0.0/16"
+  # VPC (Virtual Private Cloud) configuration.
+  vpc1_name = "main-vpc"      # Name identifier of the VPC.
+  vpc1_cidr = "172.20.0.0/16" # Subnet of the whole VPC.
 
-  sub1_name = "main-priv-sub"
-  sub1_cidr = "172.20.0.0/24"
+  # Subnet configuration.
+  sub1_name = "main-public-sub" # Name identifier of the subnet.
+  sub1_cidr = "172.20.0.0/24"   # Cidr of the subnet.
 
-  sub2_name = "main-priv-sub"
-  sub2_cidr = "172.20.1.0/24"
+  # Subnet configuration.
+  sub2_name = "main-privat-sub" # Name identifier of the subnet.
+  sub2_cidr = "172.20.1.0/24"   # Cidr of the subnet.
 
-  key1_name = "Laptop"
-  key1_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhtELWTC/hIFrkkyEAtQbozzMv2YT0D08A+Bsbph9Fi5LEg8flZR50kBmqLRHQp0oU30Vm6oNuJkcxahieH5T1NnmgivBpk9R6gBFwPoUoZ9aqbMF4OzJmN27Zci6OhU/f0qlvZ0u+uEBk5jNuVvwbkXNq+SaZazq8Neo6tvepfgxwYg8VKblOqFWHdsuGMzhFGe6fPqAnAxCyOXZPP1TWQ2ggk6w2h9MM4KBF/gezZz5YmriIHiI+v0erZSIcC9HoD+sfzJ0ke0A5CQT9Ab/HwAfGyIA+PdxFZ3n7kEI9/WWtm/cLMl3iQ9UH3yKqFxYE2WVyaVRvkj157bdcO+Vi9qZ+KiOCJffq9aaBmNAs1v0uQxHe5syj8THtX/dQgeEewvD9wPnDMmOjZCpGtkFKf4iXuvZkMPQoQPe9fwBKzb4KaWf+ImqK0UuwAuwOcOB4rsJ1PHA6EnuYQWsn7sO1HljRtG+ODrmejytLr/AKRVxLyxSMywAeMWACPh+89JE= main\\manuel.regli@FENNB04206"
+  # NAT-Instance configuration.
+  nat1_name = "main-nat-instance" # Name of the Nat-Instance
+
+  # Key pair configuration.
+  key1_name = "Laptop" # Name of the key pair
+  key1_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhtELWTC/hIFrkkyEAtQbozzMv2YT0D08A+Bsbph9Fi5LEg8flZR50kBmqLRHQp0oU30Vm6oNuJkcxahieH5T1NnmgivBpk9R6gBFwPoUoZ9aqbMF4OzJmN27Zci6OhU/f0qlvZ0u+uEBk5jNuVvwbkXNq+SaZazq8Neo6tvepfgxwYg8VKblOqFWHdsuGMzhFGe6fPqAnAxCyOXZPP1TWQ2ggk6w2h9MM4KBF/gezZz5YmriIHiI+v0erZSIcC9HoD+sfzJ0ke0A5CQT9Ab/HwAfGyIA+PdxFZ3n7kEI9/WWtm/cLMl3iQ9UH3yKqFxYE2WVyaVRvkj157bdcO+Vi9qZ+KiOCJffq9aaBmNAs1v0uQxHe5syj8THtX/dQgeEewvD9wPnDMmOjZCpGtkFKf4iXuvZkMPQoQPe9fwBKzb4KaWf+ImqK0UuwAuwOcOB4rsJ1PHA6EnuYQWsn7sO1HljRtG+ODrmejytLr/AKRVxLyxSMywAeMWACPh+89JE= main\\manuel.regli@FENNB04206"
 }
--- env/nop/moduls.tf
+++ env/nop/moduls.tf
@@ -1,71 +1,77 @@
-# Use the 'virtual-machine' module to provision a VM instance with the defined properties.
+# Use the 'vpc' module to provision a VPC with the defined properties.
 module "vpc1" {
-  source = "../../modules/vpc" # Relative path to the virtual machine module.
-  name   = local.vpc1_name     # The name of the VM instance.
-  cidr   = local.vpc1_cidr
+  source = "../../modules/vpc" # Relative path to the vpc module.
+  name   = local.vpc1_name     # The name of the vpc
+  cidr   = local.vpc1_cidr     # The cidr of the vpc
 }
 
+# Use the 'subnet' module to provision a subnet with the defined properties.
 module "sub1" {
-  source = "../../modules/subnet" # Relative path to the virtual machine module.
-  name   = local.sub1_name        # The name of the VM instance.
-  cidr   = local.sub1_cidr
-  vpc_id = module.vpc1.info.id
+  source = "../../modules/subnet" # Relative path to the subnet module.
+  name   = local.sub1_name        # The name of the subnet.
+  cidr   = local.sub1_cidr        # The cidr of the subnet.
+  vpc_id = module.vpc1.info.id    # The VPC id where the subnet should be created at.
   depends_on = [
     module.vpc1
   ]
 }
 
+# Use the 'subnet' module to provision a subnet with the defined properties.
 module "sub2" {
-  source  = "../../modules/subnet" # Relative path to the virtual machine module.
-  name    = local.sub2_name        # The name of the VM instance.
-  cidr    = local.sub2_cidr
-  vpc_id  = module.vpc1.info.id
-  private = true
+  source  = "../../modules/subnet" # Relative path to the subnet module.
+  name    = local.sub2_name        # The name of the subnet.
+  cidr    = local.sub2_cidr        # The cidr of the subnet.
+  vpc_id  = module.vpc1.info.id    # The VPC id where the subnet should be created at.
+  private = true                   # Boolean that the subnet should be private (default is false).
   depends_on = [
     module.vpc1
   ]
 }
 
+# Use the 'nat-instance' module to provision a NAT Instance with the defined properties.
 module "nat1" {
-  source          = "../../modules/nat-instance"
-  name            = "test"
-  subnet_id_place = module.sub1.info.id
-  subnet_id_route = module.sub2.info.id
-  vpc_id          = module.vpc1.info.id
+  source          = "../../modules/nat-instance" # Relative path to the nat-instance module.
+  name            = local.nat1_name              # The name of the NAT Instance.
+  subnet_id_place = module.sub1.info.id          # The subnet where the NAT Instance should be created in.
+  subnet_id_route = module.sub2.info.id          # The subnet which should route all external traffic to the NAT Instance
+  vpc_id          = module.vpc1.info.id          # The VPC id of the VPC in which the NAT Instance should be
   depends_on = [
     module.vpc1, module.sub1
   ]
 }
 
+# Use the 'key_pair' module to provision a key pair with the defined properties.
 module "key1" {
-  source          = "../../modules/key_pair"
-  name            = local.key1_name
-  key = local.key1_key
+  source = "../../modules/key_pair" # Relative path to the key_pair module.
+  name   = local.key1_name          # The name of the key pair
+  key    = local.key1_key           # The public ssh key for the key pair
 }
 
+# Use the 'virtual-machine' module to provision a Key Pair with the defined properties.
 module "vm1" {
-  source    = "../../modules/virtual-machine"
-  name      = local.vm1_name
-  image     = local.vm1_image
-  type      = local.vm1_type
-  disk_size = local.vm1_disk_size
-  disk_type = local.vm1_disk_type
-  subnet_id = module.sub1.info.id
-  key_pair = local.key1_name
+  source    = "../../modules/virtual-machine" # Relative path to the virtual-machine module.
+  name      = local.vm1_name                  # The name of the Virtual Machine
+  image     = local.vm1_image                 # The image of the Virtual Machine
+  type      = local.vm1_type                  # The VM-Type of the Virtual Machine
+  disk_size = local.vm1_disk_size             # The disk size of the Virtual Machine
+  disk_type = local.vm1_disk_type             # The disk type of the Virtual Machine
+  subnet_id = module.sub1.info.id             # The subnet in which the Virtual Machine should be placed in.
+  key_pair  = local.key1_name                 # The ssh key the Virtual Machine should use.
   depends_on = [
     module.sub1, module.key1
   ]
 }
 
+# Use the 'virtual-machine' module to provision a Key Pair with the defined properties.
 module "vm2" {
-  source    = "../../modules/virtual-machine"
-  name      = local.vm2_name
-  image     = local.vm2_image
-  type      = local.vm2_type
-  disk_size = local.vm2_disk_size
-  disk_type = local.vm2_disk_type
-  subnet_id = module.sub2.info.id
-  key_pair = local.key1_name
+  source    = "../../modules/virtual-machine" # Relative path to the virtual-machine module.
+  name      = local.vm2_name                  # The name of the Virtual Machine
+  image     = local.vm2_image                 # The image of the Virtual Machine
+  type      = local.vm2_type                  # The VM-Type of the Virtual Machine
+  disk_size = local.vm2_disk_size             # The disk size of the Virtual Machine
+  disk_type = local.vm2_disk_type             # The disk type of the Virtual Machine
+  subnet_id = module.sub2.info.id             # The subnet in which the Virtual Machine should be placed in.
+  key_pair  = local.key1_name                 # The ssh key the Virtual Machine should use.
   depends_on = [
     module.sub2, module.key1
   ]
