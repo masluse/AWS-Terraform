ff0cd5c56107397a4284d31c80c46866aa3a7c2c
masluse <122784119+masluse@users.noreply.github.com>
2023-11-22 10:37:14 +0000

    22.11.2023

--- modules/ansible/main.tf
+++ modules/ansible/main.tf
@@ -10,4 +10,12 @@ resource "null_resource" "ansible_provisioner" {
     # The command to run the Ansible playbook with the provided inventory, private key, extra vars, and playbook path.
   }
   depends_on = [time_sleep.wait_40_seconds]
+
+  # Trigger für die Neuausführung hinzufügen, basierend auf den Werten von ansible_extra_vars
+  triggers = {
+    ansible_vars_hash = md5(jsonencode({
+      ansible_extra_vars = local.ansible_extra_vars_command,
+      vm_name = var.public_ip
+    })) 
+  }
 }
